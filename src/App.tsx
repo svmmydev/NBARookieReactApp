cbf69bab581058dbe923513a50458d918ed75119
Sammy <svmmydev@gmail.com>
2025-04-11 21:15:15 +0200

    Hook: Implement useFavorites

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Login from './pages/Login';
+import Favorites from './pages/Favorites';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -41,6 +42,9 @@ const App: React.FC = () => (
       <IonRouterOutlet>
         <Route exact path="/login">
           <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
         </Route>
         <Route exact path="/">
           <Redirect to="/login" />
